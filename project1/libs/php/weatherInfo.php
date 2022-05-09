7f1ad43cf60ccddbe2b7c354f5f02509717e8614
Alex <100403601+EnolFlow@users.noreply.github.com>
2022-05-09 13:43:48 +0100

    Update weatherInfo.php

--- project1/libs/php/weatherInfo.php
+++ project1/libs/php/weatherInfo.php
@@ -2,6 +2,6 @@
 header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
 $lat = $_REQUEST['lat'];
 $lng = $_REQUEST['lng'];
-$data = file_get_contents("https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lng&exclude=hourly,daily,minutely&appid=6e1abe8d6a74c560d636cae6e03d824b&units=metric");
+$data = file_get_contents("https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lng&exclude=hourly,minutely&appid=6e1abe8d6a74c560d636cae6e03d824b&units=metric");
 print_r($data);
 ?>
